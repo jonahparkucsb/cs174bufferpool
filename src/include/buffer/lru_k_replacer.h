5c056a90a590bad2e037370fd58acd9b56873f8e
jonahparkucsb <77710873+jonahparkucsb@users.noreply.github.com>
2024-06-07 18:42:23 -0700

    Add files via upload

--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -23,18 +23,7 @@
 
 namespace bustub {
 
-enum class AccessType { Unknown = 0, Lookup, Scan, Index };
-
-class LRUKNode {
- private:
-  /** History of last seen K timestamps of this page. Least recent timestamp stored in front. */
-  // Remove maybe_unused if you start using them. Feel free to change the member variables as you want.
-
-  [[maybe_unused]] std::list<size_t> history_;
-  [[maybe_unused]] size_t k_;
-  [[maybe_unused]] frame_id_t fid_;
-  [[maybe_unused]] bool is_evictable_{false};
-};
+constexpr frame_id_t INVALID_FRAME_ID = std::numeric_limits<frame_id_t>::max();
 
 /**
  * LRUKReplacer implements the LRU-k replacement policy.
@@ -74,8 +63,8 @@ class LRUKReplacer {
    * that are marked as 'evictable' are candidates for eviction.
    *
    * A frame with less than k historical references is given +inf as its backward k-distance.
-   * If multiple frames have inf backward k-distance, then evict frame with earliest timestamp
-   * based on LRU.
+   * If multiple frames have inf backward k-distance, then evict the frame with the earliest
+   * timestamp overall.
    *
    * Successful eviction of a frame should decrement the size of replacer and remove the frame's
    * access history.
@@ -95,10 +84,8 @@ class LRUKReplacer {
    * also use BUSTUB_ASSERT to abort the process if frame id is invalid.
    *
    * @param frame_id id of frame that received a new access.
-   * @param access_type type of access that was received. This parameter is only needed for
-   * leaderboard tests.
    */
-  void RecordAccess(frame_id_t frame_id, AccessType access_type = AccessType::Unknown);
+  void RecordAccess(frame_id_t frame_id);
 
   /**
    * TODO(P1): Add implementation
@@ -150,12 +137,18 @@ class LRUKReplacer {
  private:
   // TODO(student): implement me! You can replace these member variables as you like.
   // Remove maybe_unused if you start using them.
-  [[maybe_unused]] std::unordered_map<frame_id_t, LRUKNode> node_store_;
-  [[maybe_unused]] size_t current_timestamp_{0};
-  [[maybe_unused]] size_t curr_size_{0};
-  [[maybe_unused]] size_t replacer_size_;
-  [[maybe_unused]] size_t k_;
-  [[maybe_unused]] std::mutex latch_;
+  size_t current_timestamp_{0};
+  size_t curr_size_{0};
+  size_t replacer_size_;
+  size_t k_;
+  std::mutex latch_;
+
+  struct FrameInfo {
+    bool evictable{false};
+    std::list<size_t> timestamp;
+  };
+
+  std::unordered_map<frame_id_t, FrameInfo> frame_table_;
 };
 
 }  // namespace bustub
